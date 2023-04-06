0f5d3445c0da50affb6c745c8111774f77d67ec9
asawant01 <130009253+asawant01@users.noreply.github.com>
2023-04-05 20:38:30 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
 import streamlit as st
-num = 4
-st.write('Square of ',num,'is',num**2)
-if __name__ == '__main__':
-  main()
+num = st.slider('Choose your number', 1,100)
+st.write('Square of ', num, 'is', num**2)
